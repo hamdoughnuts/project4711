826dadc84fc151cea4e6db976ad26dd9950faac0
hamdoughnuts <hamdoughnuts@gmail.com>
2014-11-02 18:36:01 -0800

    Added comments to admin

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -1,8 +1,7 @@
 <?php
 
 /**
- * Our homepage. Shows the most recent attraction by date and 3 buttons for the 
- * eat, play and sleep categories.
+ * Our admin page. 
  * 
  */
 class Admin extends Application {
@@ -11,15 +10,15 @@ class Admin extends Application {
         parent::__construct();
     }
 
-    /*
-     * Renders the homepage with the most recent attraction.
-     */
-
+    // renders admin page showing the id, category, name and date added
     function index() {
-        $this->data['pagebody'] = 'admin';    // this is the view we want shown
+        // this is the view we want shown
+        $this->data['pagebody'] = 'admin';
+
         // build the list of attractions, to pass on to our view
         $source = $this->attractions->getAll();
         $attractions = array();
+
         foreach ($source as $record) {
             $attractions[] = array(
                 'id' => $record->id
@@ -29,66 +28,69 @@ class Admin extends Application {
             );
         }
         $this->data['attractions'] = $attractions;
-
         $this->render();
     }
 
+    // renders an edit page for a single attraction
     function edit($id) {
         $this->data['pagebody'] = 'edit';
         $this->load->library('session');
 
-        // use “item” as the session key
-        // assume no item record in-progress
+        // using "item" as the session key
+        // assuming no item record in-progress
         $item_record = null;
-        // do we have an item in the session already {
+
+        // check if we have an item in the session already
         $session_record = $this->session->userdata('item');
         if ($session_record !== FALSE) {
-            // does its item # match the requested one {
+            // does if id matches the requested one
             if (isset($session_record->id) && ($session_record->id == $id)) {
-                // use the item record from the session
+                // if it matches use the item record from the session
                 $item_record = $session_record;
             }
         }
-        // if no item-in progress record {
+
+        // if there is no item in the session
         if ($item_record == null) {
-            // get the item record from the items model
+            // get the item record from the items model if it exists
             if ($this->attractions->exists($id))
                 $item_record = $this->attractions->get($id);
+            // else create an item record with the id
             else {
                 $item_record = $this->attractions->create();
                 $item_record->id = $id;
             }
-                
             // save it as the “item” session object
             $this->session->set_userdata('item', $item_record);
         }
 
-            $this->data['id'] = $item_record->id;
-            $this->data['category'] = $item_record->category;
-            $this->data['name'] = $item_record->name;
-            $this->data['image'] = $item_record->image1;
-            $this->data['image2'] = $item_record->image2;
-            $this->data['image3'] = $item_record->image3;
-            $this->data['contact'] = $item_record->contact;
-            $this->data['address'] = $item_record->address;
-            $this->data['longtext'] = $item_record->longtext;
-            $this->data['shorttext'] = $item_record->shorttext;
-            $this->data['most_popular'] = $item_record->most_popular_dish;
-            $this->data['single_room_rate'] = $item_record->single_room_rating;
-            $this->data['double_room_rate'] = $item_record->double_room_rating;
-            $this->render();
-   
+        // set the view parameterss with the current item record
+        $this->data['id'] = $item_record->id;
+        $this->data['category'] = $item_record->category;
+        $this->data['name'] = $item_record->name;
+        $this->data['image'] = $item_record->image1;
+        $this->data['image2'] = $item_record->image2;
+        $this->data['image3'] = $item_record->image3;
+        $this->data['contact'] = $item_record->contact;
+        $this->data['address'] = $item_record->address;
+        $this->data['longtext'] = $item_record->longtext;
+        $this->data['shorttext'] = $item_record->shorttext;
+        $this->data['most_popular'] = $item_record->most_popular_dish;
+        $this->data['single_room_rate'] = $item_record->single_room_rating;
+        $this->data['double_room_rate'] = $item_record->double_room_rating;
+        $this->render();
     }
 
+    // handle an attraction edit form submission
     function post($id) {
+        // config uploader settings and load helpers and libraries
         $config['upload_path'] = 'data';
         $config['allowed_types'] = 'gif|jpg|png|jpeg|pdf|doc|xml';
         $config['overwrite'] = 'TRUE';
         $this->load->helper(array('form', 'url'));
         $this->load->library('upload', $config);
 
-        $fields = $this->input->post(); // gives us an associative array
-        // validation section
+        // validating fields
         if (empty($_POST['name'])) {
             $this->errors [] = 'Name cannot be left blank';
         }
@@ -110,25 +112,21 @@ class Admin extends Application {
 
         // get the session item record
         $to_update = $this->session->userdata('item');
-        // merge the session record into the model item record, over-riding any edited fields
+        // over-riding any edited fields in the session record
         if ($this->attractions->exists($id))
             $to_update = $this->attractions->getByID($id);
 
         $to_update->name = $_POST['name'];
         $to_update->category = $_POST['category'];
-
         if ($this->upload->do_upload('image1')) {
             $to_update->image1 = $_FILES['image1']['name'];
         }
-
         if ($this->upload->do_upload('image2')) {
             $to_update->image2 = $_FILES['image2']['name'];
         }
-
         if ($this->upload->do_upload('image3')) {
             $to_update->image3 = $_FILES['image3']['name'];
         }
-
         $to_update->longtext = $_POST['longtext'];
         $to_update->shorttext = $_POST['shorttext'];
         $to_update->contact = $_POST['contact'];
@@ -136,26 +134,31 @@ class Admin extends Application {
         $to_update->most_popular_dish = $_POST['most_popular'];
         $to_update->single_room_rating = $_POST['single_room_rate'];
         $to_update->double_room_rating = $_POST['double_room_rate'];
-        // update the session
+        // update the session item
         $this->session->set_userdata('item', $to_update);
 
+        // update or create if ok
         if (count($this->errors) < 1) {
-
-            // check if update or create
+            // check if updating
             if ($this->attractions->exists($id)) {
+                // update record in db
                 $this->attractions->update($to_update);
-                $this->session->unset_userdata('item');
+                // remove the item record from the session container
                 $this->session->unset_userdata('item');
                 redirect("/admin");
             } else {
+                // set id
                 $to_update->id = $id;
                 // date will be for when attraction is added
                 $to_update->date = date('Y/m/d h:i:s', time());
+                // add the attraction
                 $this->attractions->add($to_update);
+                // remove the item record from the session container
                 $this->session->unset_userdata('item');
                 redirect("/admin");
             }
         } else {
+            // update or create is not ok redirect to edit page
             $this->edit($id);
         }
     }
@@ -168,7 +171,7 @@ class Admin extends Application {
         redirect('/admin/edit/' . $id);
     }
 
-    // deletes an attraction from the database table
+    // deletes an attraction from the db table
     function delete($id) {
         $this->data['pagebody'] = 'admin';
 
@@ -185,7 +188,6 @@ class Admin extends Application {
         if (file_exists(FCPATH . 'data/' . $record->image3))
             unlink(FCPATH . 'data/' . $record->image3);
 
-        // render page
         $this->render();
     }
 
