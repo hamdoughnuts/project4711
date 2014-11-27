1d3e2e242c475ae77f89156d0cf8a7425e2974ba
hamdoughnuts <hamdoughnuts@gmail.com>
2014-11-27 09:13:18 -0800

    nov 27
    - facebook garbage

--- application/views/eat.php
+++ application/views/eat.php
@@ -1,3 +1,11 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 
 <div class="container-fluid">
     <div class="row-fluid top-buffer">
@@ -24,7 +32,10 @@
             <span class="whiteWords">{address}</span>
             <br/>
             <span class="whiteWords"><b>Our most popular dish: {most_popular}</b></span>
-
+            
+            <div class="fb-like" data-href="http://www.bcit.ca/" data-layout="standard" data-action="like" data-show-faces="true" data-share="false"></div>
+            <div class="fb-share-button" data-href="http://www.bcit.ca/" data-layout="button_count"></div>
+            <div class="fb-follow" data-href="https://www.facebook.com/bcit.ca" data-width="5" data-colorscheme="light" data-layout="standard" data-show-faces="true"></div>
         </div>
 
         <div class="col-md-12 top-buffer whiteBox">
