20e767131095ee779afebaaac11ab2f97da37c06
Danny Tran <dannyhaitran@outlook.com>
2014-12-07 13:25:18 -0800

    more styling changes

--- application/views/about.php
+++ application/views/about.php
@@ -5,24 +5,31 @@
 <div id="page-content-wrapper">
     <div class="container-fluid">
         <div class="row">
-            <div class="whiteBox">
-                <p class="whiteWords">The Looks :
+            <div class="whiteBox col-xs-12">
+                <div class="col-xs-10 col-xs-offset-1 top-buffer">
+                    <p class="whiteWords"><strong>The Looks</strong> :
                     For this project, we’ve dived right in and created a personalized website using CodeIginiter. For
                     the two of us, it’s our first time getting our hands dirty with Php. We decided to use the power of
                     Bootstrap and additional CSS3 libraries to style the web app.</p>
+            
+                </div>
             </div>
-            <div class="whiteBox">
-                <p class="whiteWords">Database :
+            <div class="whiteBox col-xs-12">
+                <div class="col-xs-10 col-xs-offset-1">
+                    <p class="whiteWords"><strong>Database</strong> :
                     For this particular web app, we’re going to stick to using the relational database to handle our
                     data. By using a relational database, we can entail that our data will be robust and full of
                     integrity. When we insert a row of data, we can expect that to be the last time we will do that
                     because the data only needs to be stored once, this is efficient and a much easier job to maintain
                     security</p>
+                </div>
             </div>
-            <div class="whiteBox">
-                <p class="whiteWords">Management :
+            <div class="whiteBox col-xs-12">
+                <div class="col-xs-10 col-xs-offset-1">
+                    <p class="whiteWords"><strong>Management</strong> :
                     We turned to Github to manage how we will be working on the project, that way we won’t run into
                     terrible conflicts.</p>
+                </div>
             </div>
         </div>
     </div>
